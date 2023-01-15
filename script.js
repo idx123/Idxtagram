8e24034d999e8a121eff239a99149b01db76cc18
idx123 <jinhalee020@gmail.com>
2023-01-15 19:50:38 +0900

    [Fix] script.js 주석 처리 제거

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// //로그인 페이지
-// const validButton = document.querySelector('.login-button');
-// const emailField = document.querySelector('#email');
-// const pwField = document.querySelector('#password');
+//로그인 페이지
+const validButton = document.querySelector('.login-button');
+const emailField = document.querySelector('#email');
+const pwField = document.querySelector('#password');
 
-// const toggle =()=>{
-//     if(emailField.value && pwField.value){
-//         validButton.disabled = false;
-//         validButton.classList.add('login-btn-changed');
-//     }else{
-//         validButton.disabled = true;
-//         validButton.classList.remove('login-btn-changed');
-//     }
-// }
+const toggle =()=>{
+    if(emailField.value && pwField.value){
+        validButton.disabled = false;
+        validButton.classList.add('login-btn-changed');
+    }else{
+        validButton.disabled = true;
+        validButton.classList.remove('login-btn-changed');
+    }
+}
 
-// emailField.onkeyup = toggle;
-// pwField.onkeyup = toggle;
-// validButton.addEventListener('click', ()=>{
-//     alert('서버가 없어서 로그인은 안되지롱');
-// })
+emailField.onkeyup = toggle;
+pwField.onkeyup = toggle;
+validButton.addEventListener('click', ()=>{
+    alert('서버가 없어서 로그인은 안되지롱');
+})
 
 //프로필 페이지
 let radioBtns = document.querySelectorAll('input[type="radio"]');

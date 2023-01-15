242c688c69719dc92528873b2f7533dba9b91041
idx123 <jinhalee020@gmail.com>
2023-01-15 19:48:24 +0900

    [Fix] 디렉터리 구조 전면 수정 및 네비게이션바,프로필페이지 완성

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const validButton = document.querySelector('.login-button');
-const emailField = document.querySelector('#email');
-const pwField = document.querySelector('#password');
+// //로그인 페이지
+// const validButton = document.querySelector('.login-button');
+// const emailField = document.querySelector('#email');
+// const pwField = document.querySelector('#password');
 
-const toggle =(e)=>{
-    // e.preventDefault();
-    if(emailField.value && pwField.value){
-        validButton.disabled = false;
-        validButton.classList.add('login-btn-changed');
-    }else{
-        validButton.disabled = true;
-        validButton.classList.remove('login-btn-changed');
-    }
+// const toggle =()=>{
+//     if(emailField.value && pwField.value){
+//         validButton.disabled = false;
+//         validButton.classList.add('login-btn-changed');
+//     }else{
+//         validButton.disabled = true;
+//         validButton.classList.remove('login-btn-changed');
+//     }
+// }
+
+// emailField.onkeyup = toggle;
+// pwField.onkeyup = toggle;
+// validButton.addEventListener('click', ()=>{
+//     alert('서버가 없어서 로그인은 안되지롱');
+// })
+
+//프로필 페이지
+let radioBtns = document.querySelectorAll('input[type="radio"]');
+
+radioBtns.forEach(i=>{ 
+    i.addEventListener('click', (e)=>{
+        activate(e.target);
+    })
+});
+
+function activate(radio){
+    let conArr = document.querySelectorAll('.contents');
+    conArr.forEach(i =>{ 
+        i.classList.remove('active');
+    });
+    let con = document.querySelector(`.${radio.id}`);
+    con.classList.add('active');
 }
-
-emailField.onkeyup = toggle;
-pwField.onkeyup = toggle;
-validButton.addEventListener('click', ()=>{
-    alert('서버가 없어서 로그인은 안되지롱');
-})
